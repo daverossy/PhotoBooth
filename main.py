4ffb3e8c6b5f8764f46869847c6dcc84357ffb6f
Dave Rossiter <david.rossiter@outlook.com>
2016-10-16 23:20:16 +0100

    Started debugging changes, screen only appears momentarily.

--- main.py
+++ main.py
@@ -40,185 +40,189 @@ background = background.convert()  # Convert it to a background
 
 # Main Thread
 def main(threadName, *args):
-    # Setup GPIO pins
-    gpio.setup(24, gpio.IN)  # Button on Pin 24 Reprints last image
-    gpio.setup(22, gpio.IN)  # Button on Pin 22 is the shutter
 
-    # Setup global variables
-    global closeme
-    global timepulse
-    global TotalImageCount
-    global Numeral
-    global SmallMessage
-    global Message
 
-    # Update message to loading
-    Message = "Loading..."
+# Setup GPIO pins
+gpio.setup(24, gpio.IN)  # Button on Pin 24 Reprints last image
+gpio.setup(22, gpio.IN)  # Button on Pin 22 is the shutter
 
-    # Update display to reflect message
-    update_display(TotalImageCount, Numeral, Message, PhotosPerCart, screen, background, pygame)
+# Setup global variables
+global closeme
+global timepulse
+global TotalImageCount
+global Numeral
+global SmallMessage
+global Message
 
-    # 5 Second delay to allow USB to mount
-    time.sleep(5)
+# Update message to loading
+Message = "Loading..."
 
-    # Call camera initialisation function
-    Camera.initialise_camera()
+# Update display to reflect message
+update_display(TotalImageCount, Numeral, Message, PhotosPerCart, screen, background, pygame)
 
-    # Start camera preview
-    Camera.start_preview()
+# 5 Second delay to allow USB to mount
+time.sleep(5)
 
-    # Initialise the external storage
-    ExternalStorage.initialise()
+# Call camera initialisation function
+Camera.initialise_camera()
 
-    # Set message to initialise
-    Message = "Initialise"
+# Start camera preview
+Camera.start_preview()
 
-    # Update display to reflect message
-    update_display(Message, PhotosPerCart)
+# Initialise the external storage
+ExternalStorage.initialise()
 
-    # Procedure checks if a numerical folder exists, if it does pick the next number
-    # each start gets a new folder i.e. /photobooth/1/ etc
-    ExternalStorage.folder_check()
+# Set message to initialise
+Message = "Initialise"
 
-    # Set message to empty
-    Message = ""
+# Update display to reflect message
+update_display(Message, PhotosPerCart)
 
-    # Update display to reflect message
-    update_display(Message, PhotosPerCart)
+# Procedure checks if a numerical folder exists, if it does pick the next number
+# each start gets a new folder i.e. /photobooth/1/ etc
+ExternalStorage.folder_check()
 
-    # Main Loop
-    while closeme:
-        try:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+# Set message to empty
+Message = ""
+
+# Update display to reflect message
+update_display(Message, PhotosPerCart)
+
+# Main Loop
+while closeme:
+    try:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                closeme = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
                     closeme = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        closeme = False
-        except KeyboardInterrupt:
-            closeme = False
+    except KeyboardInterrupt:
+        closeme = False
 
-        # input_value is the shuttergp
-        gpio.setup(22, gpio.IN)
-        input_value = gpio.input(22)
-        # input_value2 is photo reprint
-        gpio.setup(24, gpio.IN)
-        input_value2 = gpio.input(24)
+    # input_value is the shuttergp
+    gpio.setup(22, gpio.IN)
+    input_value = gpio.input(22)
+    # input_value2 is photo reprint
+    gpio.setup(24, gpio.IN)
+    input_value2 = gpio.input(24)
 
-    # Update display to reflect changes
-    update_display(Message, PhotosPerCart)
+# Update display to reflect changes
+update_display(Message, PhotosPerCart)
 
-    # Reprint Button has been pressed
-    if input_value2 == False:
-        # If the temp image exists send it to the printer
-        if os.path.isfile('/home/pi/Desktop/tempprint.jpg'):
-            # Call reprint function
-            Print.reprint()
-            # Wait 20 seconds
-            time.sleep(20)
-            # Set message to empty
-            Message = ""
-            # Update display to reflect changes
-            update_display(PhotosPerCart)
-
-    # input_value is the shutter release
-    if input_value == False:
-        subimagecounter = 0
-
-    # Increment the image number
-    imagecounter = imagecounter + 1
-
-    # Initialise number of shots variable
-    shotscountdown = 4
-
-    # Define empty dictionary for image variable data
-    im = {}
-
-    # Keep running until number of shots left is 0
-    while shotscountdown > 0:
-        # Initialise countdown variable to 5
-        countdown = 5
-
-        # Keep running until countdown for photo is 0
-        while countdown > 0:
-            # Display the countdown number
-            Numeral = countdown
-            update_display(Message, PhotosPerCart)
-            # Subtract 1 from countdown each increment
-            countdown - 1
-            # Flash the light at half second intervals
-            timepulse = 0.5
-            # Wait 1 second between beeps
-            time.sleep(1)
-
-        Numeral = ""
-
-        Message = "Smile!"
-
-        # Update display
-        update_display(Message, PhotosPerCart)
-
-        # increment the subimage
-        subimagecounter = subimagecounter + 1
-
-        # create the filename
-        filename = 'image'
-        filename += `imagecounter`
-        filename += '_'
-        filename += `subimagecounter`
-        filename += '.jpg'
-
-        # Capture image
-        Camera.capture(imagefolder, filename)
-
-        # Add an image element to the dictionary
-        im[shotscountdown] = PIL.Image.open(os.path.join(imagefolder, filename)).transpose(Image.FLIP_LEFT_RIGHT)
-
-        # Set message to get ready
-        Message = "Get Ready"
-
-        # Update display to reflect new message
-        update_display(Message, PhotosPerCart)
-
-        # Set timepulse to 999
-        timepulse = 999
-
-        # Wait for 3 seconds
-        time.sleep(3)
-
+# Reprint Button has been pressed
+if input_value2 == False:
+    # If the temp image exists send it to the printer
+    if os.path.isfile('/home/pi/Desktop/tempprint.jpg'):
+        # Call reprint function
+        Print.reprint()
+        # Wait 20 seconds
+        time.sleep(20)
         # Set message to empty
         Message = ""
+        # Update display to reflect changes
+        update_display(PhotosPerCart)
 
-        # Minus 1 off the amount of shots left
-        shotscountdown - 1
+# input_value is the shutter release
+if input_value == False:
+    subimagecounter = 0
 
-    # Call image processing function and pass in dictionary containing all images
-    ImageProcessing(im, background_template_location, imagefolder, imagecounter)
+imagecounter = 0
 
-    # Call print function to print photos
-    Print(TotalImageCount)
+# Increment the image number
+imagecounter + 1
 
-    # Clear message variable
-    Message = ""
+# Initialise number of shots variable
+shotscountdown = 4
+
+# Define empty dictionary for image variable data
+im = {}
+
+# Keep running until number of shots left is 0
+while shotscountdown > 0:
+    # Initialise countdown variable to 5
+    countdown = 5
+
+    # Keep running until countdown for photo is 0
+    while countdown > 0:
+        # Display the countdown number
+        Numeral = countdown
+        update_display(Message, PhotosPerCart)
+        # Subtract 1 from countdown each increment
+        countdown - 1
+        # Flash the light at half second intervals
+        timepulse = 0.5
+        # Wait 1 second between beeps
+        time.sleep(1)
+
+    Numeral = ""
+
+    Message = "Smile!"
 
     # Update display
+    update_display(Message, PhotosPerCart)
+
+    # increment the subimage
+    subimagecounter = subimagecounter + 1
+
+    # create the filename
+    filename = 'image'
+    filename += `imagecounter`
+    filename += '_'
+    filename += `subimagecounter`
+    filename += '.jpg'
+
+    # Capture image
+    Camera.capture(imagefolder, filename)
+
+    # Add an image element to the dictionary
+    im[shotscountdown] = PIL.Image.open(os.path.join(imagefolder, filename)).transpose(Image.FLIP_LEFT_RIGHT)
+
+    # Set message to get ready
+    Message = "Get Ready"
+
+    # Update display to reflect new message
     update_display(Message, PhotosPerCart)
 
     # Set timepulse to 999
     timepulse = 999
 
-    # Reset the shutter switch
-    while input_value == False:
-        input_value = gpio.input(22)
+    # Wait for 3 seconds
+    time.sleep(3)
 
-    # Stop preview
-    Camera.stop_preview()
+    # Set message to empty
+    Message = ""
 
-    # Launch main thread
-    Thread(target=main, args=('Main', 1)).start()
+    # Minus 1 off the amount of shots left
+    shotscountdown - 1
 
-    # Launch pulse thread
-    Thread(target=pulse, args=('Pulse', 1)).start()
+# Call image processing function and pass in dictionary containing all images
+ImageProcessing(im, background_template_location, imagefolder, imagecounter)
 
-    # Sleep for 5 seconds
-    time.sleep(5)
+# Call print function to print photos
+Print(TotalImageCount)
+
+# Clear message variable
+Message = ""
+
+# Update display
+update_display(Message, PhotosPerCart)
+
+# Set timepulse to 999
+timepulse = 999
+
+# Reset the shutter switch
+while input_value == False:
+    input_value = gpio.input(22)
+
+# Stop preview
+Camera.stop_preview()
+
+# Launch main thread
+Thread(target=main, args=('Main', 1)).start()
+
+# Launch pulse thread
+Thread(target=pulse, args=('Pulse', 1)).start()
+
+# Sleep for 5 seconds
+time.sleep(5)
